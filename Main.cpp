ec78332fe5c4ce0ff3f75d645d35c0ddcc70da8b
alstit <alexyls78@gmail.com>
2020-09-28 21:31:01 +0200

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -48,67 +48,86 @@ int main(int argc, char* args[])
 		
 		//screenflip(renderer);
 
-		float mass = .002;
-		float K = .008;
-		float damp = 0.01;
+		float mass = .005;
+		float K = .01;
+		float damp = 0.1;
 		//float mass = .2;
 		//float K = .5;
 		//float damp = 0.1;
 		//screenflip(renderer);
 		//SDL_Delay(1000);
 		//Mass(int x, int y, int size, float mass, SDL_Renderer* renderer)
-		Mass aMass2((WIDTH-100) / 2 / SCALE, (HEIGHT -20)/ 2 / SCALE, 10, mass, renderer);
+		/*Mass aMass2((WIDTH-100) / 2 / SCALE, (HEIGHT -20)/ 2 / SCALE, 10, mass, renderer);
 		Mass aMass((WIDTH )/ 2/SCALE, HEIGHT / 2/SCALE,10, mass, renderer);
 		Mass aMass3(((WIDTH -300)/ 2 / SCALE), ((HEIGHT-300)/ 2 / SCALE), 10, mass, renderer);
 		//float x, float y,float elasticity ,float damp,Mass aMass
 		Springf aSpring(&aMass2,K,damp, &aMass);
 		Springf aSpring2(&aMass3,K, damp, &aMass2);
-		
-		//vector<Mass> masses;
-		//vector<Springf> springs;
+		*/
+		Mass* masses[25];
+		vector<Springf> springsV;
+		vector<Springf> springsH;
 
-		/*
-		for (int i = 0;i<10;i++)
+		int size = 5;
+
+		for (int i = 0; i < size+1 ; i++)
 		{
-			for(int j = 0;j<10;j++)
-			{
-				Mass aMass((WIDTH - 10*j) / SCALE, (HEIGHT - 10*i) / SCALE, 10, mass, renderer);
-				masses.push_back(aMass);
-				if (j >= 1)
-				{
-					Springf aSpring(&masses[j-1], K, damp, &masses[j]);
-					springs.push_back(aSpring);
-				}
 
+			for (int j = 0; j < size+1; j++) 
+			{
+				Mass* aMass = new Mass((10 * j)+10, (10 * i)+10, 10, mass, renderer);
+				masses[i*size+j] = aMass;
+				//if (j > 0 && (j!=size && i>0))
+				if (j > 0) 
+				{
+					Springf aspring(masses[i*size + j - 1], K, damp, masses[i*size + j]);
+					springsH.push_back(aspring);
+				}
+				if (i >0)
+				{
+					Springf aspring(masses[i*size + j -size], K, damp, masses[i*size +j]);
+					springsV.push_back(aspring);
+				}
 			}
 		}
 		
-		*/
+		//Mass aMass2(100, 3, 10, mass, renderer);
+		//Springf aspring1(masses[0], K, damp, &aMass2);
+		//springsH.push_back(aspring1);
+
+
 		
 		while(true)
 		{
 			//cout << aSpring.masse.vx << endl;
 			screenflip(renderer);
-			/*for (int i = 0; i < springs.size(); i++) 
+			for (int i = 0; i < springsV.size(); i++) 
 			{
-				springs[i].update(0.001);
-			}*/
-			aSpring.update(0.001);
-			aSpring2.update(0.001);
+				springsV[i].update(0.001);
+
+						
+			}
+
+			for (int i = 0; i < springsH.size(); i++) 
+			{
+				springsH[i].update(0.001);
+			}
+			//aSpring.update(0.001);
+			//aSpring2.update(0.001);
 			SDL_RenderPresent(renderer);
 			SDL_Delay(0.1);
 		}
 
 
 
-
+			while (true) {}
 
 		if (screen)
 		{
-			aSpring.update(1);
-			SDL_Delay(1000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
-			aSpring.update(1);
-			SDL_Delay(1000);
+			//aSpring.update(1);
+			//SDL_Delay(1000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
+			//aSpring.update(1);
+			//SDL_Delay(1000);
 
 			SDL_DestroyWindow(screen);
 		}
